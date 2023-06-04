b772840b75a8126b74fb8161a452d01ff1607d9d
AHMERSHIEKH20 <133066055+AHMERSHIEKH20@users.noreply.github.com>
2023-06-04 12:38:37 +0500

    Update Ahmer.py

--- Ahmer.py
+++ Ahmer.py
@@ -1,1 +1,17 @@
+import os,platform
 
+os.system('git pull')
+
+ 
+
+Ahmer=platform.architecture()[0]
+
+if Ahmer=="32bit":
+
+    print('Sorry 32 Bit Not Supported...')
+
+elif Ahmer=="64bit":
+
+    #print('Command is in update wait we will fix it soon !')
+
+    __import__("Ahmer")
